e872f6b51c54f3928f77f8cd5ecb0cc44e83ed22
humeyraalemdar <108089138+vis-cognitionis@users.noreply.github.com>
2023-05-25 00:38:00 +0300

    Add main elements

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel = new System.Windows.Forms.Panel();
+            this.btnErase = new System.Windows.Forms.Button();
+            this.btnPen = new System.Windows.Forms.Button();
+            this.gallery = new System.Windows.Forms.Panel();
+            this.canvasPictureBox = new System.Windows.Forms.PictureBox();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvasPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.btnErase);
+            this.panel.Controls.Add(this.btnPen);
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1083, 53);
+            this.panel.TabIndex = 0;
+            // 
+            // btnErase
+            // 
+            this.btnErase.Location = new System.Drawing.Point(61, 16);
+            this.btnErase.Name = "btnErase";
+            this.btnErase.Size = new System.Drawing.Size(25, 24);
+            this.btnErase.TabIndex = 1;
+            this.btnErase.UseVisualStyleBackColor = true;
+            // 
+            // btnPen
+            // 
+            this.btnPen.Location = new System.Drawing.Point(15, 16);
+            this.btnPen.Name = "btnPen";
+            this.btnPen.Size = new System.Drawing.Size(25, 24);
+            this.btnPen.TabIndex = 0;
+            this.btnPen.UseVisualStyleBackColor = true;
+            // 
+            // gallery
+            // 
+            this.gallery.Location = new System.Drawing.Point(964, 81);
+            this.gallery.Name = "gallery";
+            this.gallery.Size = new System.Drawing.Size(130, 512);
+            this.gallery.TabIndex = 2;
+            // 
+            // canvasPictureBox
+            // 
+            this.canvasPictureBox.Location = new System.Drawing.Point(12, 81);
+            this.canvasPictureBox.Name = "canvasPictureBox";
+            this.canvasPictureBox.Size = new System.Drawing.Size(946, 512);
+            this.canvasPictureBox.TabIndex = 3;
+            this.canvasPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1107, 621);
+            this.Controls.Add(this.canvasPictureBox);
+            this.Controls.Add(this.gallery);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.canvasPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button btnErase;
+        private System.Windows.Forms.Button btnPen;
+        private System.Windows.Forms.Panel gallery;
+        private System.Windows.Forms.PictureBox canvasPictureBox;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace DrawingApp
 {
     public partial class Form1 : Form
     {
+        private bool isDrawing = false;
+        private Point previousPoint;
+
         public Form1()
         {
             InitializeComponent();
+            canvasPictureBox.Paint += CanvasPictureBox_Paint;
+            canvasPictureBox.MouseDown += CanvasPictureBox_MouseDown;
+            canvasPictureBox.MouseMove += CanvasPictureBox_MouseMove;
+            canvasPictureBox.MouseUp += CanvasPictureBox_MouseUp;
+        }
+
+        private void CanvasPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            // Çizim işlemleri burada gerçekleştirilir
+            // Örneğin, fırça rengini, kalınlığını veya diğer çizim ayarlarını belirleyebilirsiniz
+        }
+
+        private void CanvasPictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            previousPoint = e.Location;
+        }
+
+        private void CanvasPictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = canvasPictureBox.CreateGraphics())
+                {
+                    // Çizgi çizme işlemi
+                    g.DrawLine(Pens.Black, previousPoint, e.Location);
+                    previousPoint = e.Location;
+                }
+            }
+        }
+
+        private void CanvasPictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
         }
     }
 }
